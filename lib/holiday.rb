e2ed9fa0609482b3b0d1fd343166306a73586bda
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-27 21:23:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
       holiday.each do |holidays, supply|
-        puts " #{holidays.to_s.split("_").map {|x| x.capitalize}.join(" ")}: #{supply.join(", ")}"
+        puts "  #{holidays.to_s.split("_").map {|x| x.capitalize}.join(" ")}: #{supply.join(", ")}"
       end
   end
 end
